6291d09adaaee75ead2135002d4f8a26318a8ed2
kaiete <kaiete@aragon-press.com>
2021-02-16 17:25:11 +0000

    fix my mistakes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
   res.write('<h1 style="font-family: Courier;">Shhhh...</h1>');
   res.write(`<head><title>StoryTime | Node.js</title></head>`)
   res.write(`<p style="font-family: Courier;">You found a secret server, don't tell anyone!<br>This is StoryTime's node.js server - everything that goes on behind the scenes.</p>`)
-  res.end(`<footer style="font-family: Helvetica, sans-serif; position: absolute; bottom: 5px; width: 100%; color: gray; font-style: italic;"><p>Thanks for supporting storytime,<br>The StoryTime team.</p></footer>
+  res.end(`<footer style="font-family: Helvetica, sans-serif; position: absolute; bottom: 5px; width: 100%; color: gray; font-style: italic;"><p>Thanks for supporting storytime,<br>The StoryTime team.</p></footer>`)
 });
 
 server.listen(port, hostname, () => {
